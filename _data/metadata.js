1fe1452a054bfa43bc53f6cbb68e07aec3557686
Hans Kuijpers <hans2103@gmail.com>
2023-07-29 10:39:31 +0200

    adjust meta data

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,9 +1,10 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "FanfareStiphout", // No leading @ here
+	homeLabel: "fanfaredevooruitgang.nl",
+	homeUrl: "https://www.fanfaredevooruitgang.nl/",
 };
 
-data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
+//data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
+data.avatar = `/img/brandmark-fanfarestiphout.png`;
 
 module.exports = data;
